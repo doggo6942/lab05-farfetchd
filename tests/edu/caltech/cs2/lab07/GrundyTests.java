8154a0b0cf06766f8dc0d1b6806566434bc09f4d
Ethan Ordentlich <eordentl@Caltech.edu>
2021-02-15 15:01:53 -0800

    Add getMoves test and run configurations

--- tests/edu/caltech/cs2/lab07/GrundyTests.java
+++ tests/edu/caltech/cs2/lab07/GrundyTests.java
@@ -1,10 +1,17 @@
 package edu.caltech.cs2.lab07;
 
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GrundyTests {
@@ -35,10 +42,40 @@ public class GrundyTests {
         return sha256(results.toString());
     }
 
+    public static Stream<Arguments> grundysGamePositions() {
+        return Stream.of(
+                Arguments.of(3, 1, List.of("{}")),
+                Arguments.of(6, 1, List.of("{5=1}", "{4=1}")),
+                Arguments.of(7, 1, List.of("{6=1}", "{5=1}", "{3=1, 4=1}")),
+                Arguments.of(7, 2, List.of("{5=1}", "{4=1}", "{4=1}", "{3=1}", "{4=1}", "{3=2}")),
+                Arguments.of(9, 1, List.of("{8=1}", "{7=1}", "{3=1, 6=1}", "{4=1, 5=1}"))
+        );
+    }
+
+    @ParameterizedTest(name = "Test getMoves starting with heap size {0}, and making {1} moves")
+    @Tag("D")
+    @Order(0)
+    @MethodSource("grundysGamePositions")
+    @DisplayName("Test getMoves")
+    public void testGetMoves(int startSize, int moves, List<String> endingPositions) {
+        List<GrundyPosition> currentPositions = List.of(new GrundyPosition(startSize));
+        for (int i = 0; i < moves; i++) {
+            List<GrundyPosition> nextPositions = new ArrayList<>();
+            for (GrundyPosition gp : currentPositions) {
+                nextPositions.addAll(gp.getMoves());
+            }
+            currentPositions = nextPositions;
+        }
+
+        List<String> strMoves = currentPositions.stream().map(GrundyPosition::toString).collect(Collectors.toList());
+        MatcherAssert.assertThat("Incorrect positions after " + moves + " moves",
+                strMoves,
+                IsIterableContainingInAnyOrder.containsInAnyOrder(endingPositions.toArray()));
+    }
 
     @Test
     @Tag("D")
-    @Order(0)
+    @Order(1)
     @DisplayName("Test Grundy(0) to Grundy(25)")
     public void test0To25() {
         Assertions.assertEquals("c4d84ffc080ae2603869623d7837e2033ae26557da4a4b0bf97aceac70d3cbbc", hashGrundyUpTo(25));
@@ -46,7 +83,7 @@ public class GrundyTests {
 
     @Test
     @Tag("A")
-    @Order(1)
+    @Order(0)
     @DisplayName("Test Grundy(0) to Grundy(50)")
     public void test0To60() {
         Assertions.assertEquals("c9084293995ee9cf326e065f28b85a518759fd2d7ded195f257d0f19af846b9c", hashGrundyUpTo(50));
